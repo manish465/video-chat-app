2688cb13141ce35c82a72c1c533412f26e921429
manish465 <ms5978307@gmail.com>
2020-10-21 11:54:07 -0700

    added home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
 
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+
+import Home from "./pages/Home";
+
+const lightTheme = createMuiTheme({
+    palette: {
+        background: { default: "#EBEDF9" },
+        primary: { main: "#67D0E8" },
+        text: { main: "#707070" },
+    },
+});
+
 const App = () => {
-    return <div>Hello world</div>;
+    return (
+        <ThemeProvider theme={lightTheme}>
+            <Home />
+        </ThemeProvider>
+    );
 };
 
 export default App;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import {} from "@material-ui/core";
+
+const Home = () => {
+    return <div>Home</div>;
+};
+
+export default Home;
