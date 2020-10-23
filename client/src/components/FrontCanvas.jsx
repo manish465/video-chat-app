2b818bb412fead591b3c40ae0e043ba0ffd26aea
manish465 <ms5978307@gmail.com>
2020-10-22 20:45:20 -0700

    added banner componets

--- client/src/components/FrontCanvas.jsx
+++ client/src/components/FrontCanvas.jsx
@@ -1,17 +1,66 @@
 import React from "react";
 
-import { Grid, Button } from "@material-ui/core";
+import { Grid, Button, Typography, makeStyles } from "@material-ui/core";
 
 import { ReactComponent as FrontBanner } from "../asset/Banner/frontPage.svg";
 import { ReactComponent as IdGen } from "../asset/icons/genId.svg";
 import { ReactComponent as OneOne } from "../asset/icons/one-one.svg";
 import { ReactComponent as GroupConf } from "../asset/icons/groupConf.svg";
 
+const useStyles = makeStyles({
+    button: {
+        boxShadow: "3px 3px 4px 0.5px rgba(0,0,0,0.3)",
+        backgroundColor: "white",
+        padding: "5px 10px",
+        borderRadius: "10px",
+        marginBottom: "20px",
+    },
+});
+
 const FrontCanvas = () => {
+    const classes = useStyles();
+
     return (
-        <Grid container alignItems='center' justify='space-around'>
+        <Grid
+            container
+            direction='row'
+            justify='space-around'
+            alignItems='center'>
             <Grid item>
-                <FrontBanner style={{ height: "200px" }} />
+                <FrontBanner style={{ height: "550px", width: "550px" }} />
+            </Grid>
+            <Grid>
+                <Grid item>
+                    <Button
+                        style={{
+                            background:
+                                "linear-gradient(to right, #6dd5ed, #2193b0)",
+                        }}
+                        className={classes.button}>
+                        <IdGen style={{ width: "30px", marginRight: "30px" }} />
+                        <Typography variant='h6'>Create a new ID</Typography>
+                    </Button>
+                </Grid>
+                <Grid item>
+                    <Button className={classes.button}>
+                        <OneOne
+                            style={{ width: "30px", marginRight: "30px" }}
+                        />
+                        <Typography variant='h6'>
+                            One-on-One video call
+                        </Typography>
+                    </Button>
+                </Grid>
+                <Grid item>
+                    <Button className={classes.button}>
+                        <GroupConf
+                            style={{ width: "30px", marginRight: "30px" }}
+                        />
+                        <Typography variant='h6'>
+                            Group video conference
+                        </Typography>
+                    </Button>
+                </Grid>
             </Grid>
         </Grid>
     );
