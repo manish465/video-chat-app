bfc53a799d790d5306eed8a2c372054f4a520d3e
manish465 <ms5978307@gmail.com>
2020-11-16 21:22:02 -0800

    server refactor

---
+++ server/components/callPeer.js
@@ -0,0 +1,39 @@
+exports.call = (http) => {
+    const io = require("socket.io")(http);
+
+    const users = {};
+
+    io.on("connection", (socket) => {
+        console.log(`user ${socket.id} joined`);
+        if (!users[socket.id]) {
+            users[socket.id] = socket.id;
+        }
+
+        io.sockets.emit("allUsers", users);
+
+        socket.emit("yourID", socket.id);
+
+        socket.on("callUser", (data) => {
+            io.to(data.userToCall).emit("hey", {
+                signal: data.signalData,
+                from: data.from,
+            });
+        });
+
+        socket.on("acceptCall", (data) => {
+            io.to(data.to).emit("callAccepted", data.signal);
+        });
+
+        socket.on("user disconnect", () => {
+            socket.broadcast.emit("user disconnect");
+            io.sockets.emit("allUsers", users);
+        });
+
+        socket.on("disconnect", () => {
+            console.log(`user ${socket.id} left`);
+            socket.broadcast.emit("user left");
+            delete users[socket.id];
+            io.sockets.emit("allUsers", users);
+        });
+    });
+};
--- server/index.js
+++ server/index.js
@@ -1,44 +1,11 @@
 const app = require("express")();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
 
 const port = process.env.PORT || 8000;
 
-const users = {};
+const onePeer = require("./components/callPeer");
 
-io.on("connection", (socket) => {
-    console.log(`user ${socket.id} joined`);
-    if (!users[socket.id]) {
-        users[socket.id] = socket.id;
-    }
-
-    io.sockets.emit("allUsers", users);
-
-    socket.emit("yourID", socket.id);
-
-    socket.on("callUser", (data) => {
-        io.to(data.userToCall).emit("hey", {
-            signal: data.signalData,
-            from: data.from,
-        });
-    });
-
-    socket.on("acceptCall", (data) => {
-        io.to(data.to).emit("callAccepted", data.signal);
-    });
-
-    socket.on("user disconnect", () => {
-        socket.broadcast.emit("user disconnect");
-        io.sockets.emit("allUsers", users);
-    });
-
-    socket.on("disconnect", () => {
-        console.log(`user ${socket.id} left`);
-        socket.broadcast.emit("user left");
-        delete users[socket.id];
-        io.sockets.emit("allUsers", users);
-    });
-});
+onePeer.call(http);
 
 http.listen(port, () => {
     console.log(`server is running ${port}`);
